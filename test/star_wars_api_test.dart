a7d5b95504c368102bf4056359fb74a8f87db261
MeshackMusundi <meshackmusundi@gmail.com>
2019-01-02 19:52:37 +0300

    Updated test

--- test/star_wars_api_test.dart
+++ test/star_wars_api_test.dart
@@ -1,31 +1,31 @@
+import 'package:flutter_mvvm_example/models/character.dart';
+import 'package:flutter_mvvm_example/models/film.dart';
+import 'package:flutter_mvvm_example/models/planet.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_mvvm_example/interfaces/i_star_wars_api.dart';
 import 'package:flutter_mvvm_example/services/swapi_service.dart';
 
 void main() {
-  IStarWarsApi swapi = SwapiService();
+  final IStarWarsApi svc = SwapiService();
 
   test(
     'Films GET request test',
     () async {
-      var data = await swapi.getFilms();
-      expect(data.length, 7);
+      expect(await svc.getFilms(), isInstanceOf<List<Film>>());
     },
   );
 
   test(
     'Characters GET request test',
-    () async {      
-      var data = await swapi.getCharacters();
-      expect(data.length, 10);
+    () async {
+      expect(await svc.getCharacters(), isInstanceOf<List<Character>>());
     },
   );
 
   test(
     'Planets GET request test',
-    () async {      
-      var data = await swapi.getPlanets();
-      expect(data.length, 10);
+    () async {
+      expect(await svc.getPlanets(), isInstanceOf<List<Planet>>());
     },
   );
 }
